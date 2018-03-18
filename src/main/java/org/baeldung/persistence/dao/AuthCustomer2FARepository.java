5e331d4fbcb9800e48bddb95b4d8c72e03486434
Aleksandr Lazarchuk <aleksandr.lazarchuk@escalibre.net>
2018-03-19 00:15:24 +0200

    dev

--- src/main/java/org/baeldung/persistence/dao/AuthCustomer2FARepository.java
+++ src/main/java/org/baeldung/persistence/dao/AuthCustomer2FARepository.java
@@ -1,6 +1,6 @@
 package org.baeldung.persistence.dao;
 
-import org.baeldung.persistence.model.AuthUser;
+import org.baeldung.persistence.model.AuthUser2FA;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
@@ -12,7 +12,7 @@ public class AuthCustomer2FA {
     private final Logger logger = LoggerFactory.getLogger(AuthCustomer2FA.class);
 
     @Cacheable(cacheNames = "tasks", key = "#token")
-    public AuthUser findByToken(String token, AuthUser dto) {
+    public AuthUser2FA findByToken(String token, AuthUser2FA dto) {
         logger.debug("> load user to cache (when the user was successfully authentication)");
         return dto;
     }
